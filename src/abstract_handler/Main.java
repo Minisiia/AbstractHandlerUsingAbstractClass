e0609d22d4d24f4f8675f38c17c39d114fb3638b
Minisiia <ar_tik@yahoo.com>
2022-11-29 13:24:45 +0200

    Java Essential. 4. Abstract classes and interfaces

--- src/abstract_handler/Main.java
+++ src/abstract_handler/Main.java
@@ -1,24 +1,34 @@
 package abstract_handler;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        XMLHandler xmlHandler = new XMLHandler();
-        TXTHandler txtHandler = new TXTHandler();
-        DOCHandler docHandler = new DOCHandler();
-        System.out.println("XML");
-        xmlHandler.open();
-        xmlHandler.change();
-        xmlHandler.create();
-        xmlHandler.save();
-        System.out.println("TXT");
-        txtHandler.open();
-        txtHandler.change();
-        txtHandler.create();
-        txtHandler.save();
-        System.out.println("DOC");
-        docHandler.open();
-        docHandler.change();
-        docHandler.create();
-        docHandler.save();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть назву документа з розширенням:");
+        String name = scanner.next();
+        String[] arrayName = name.split("[.]");
+        String[] arrayDocumentTypes = {"xml", "txt", "doc"};
+        if (arrayName[1].equals(arrayDocumentTypes[0])) {
+            XMLHandler xmlHandler = new XMLHandler();
+            handler(xmlHandler);
+        }
+        if (arrayName[1].equals(arrayDocumentTypes[1])) {
+            TXTHandler txtHandler = new TXTHandler();
+            handler(txtHandler);
+        }
+        if (arrayName[1].equals(arrayDocumentTypes[2])) {
+            DOCHandler docHandler = new DOCHandler();
+            handler(docHandler);
+        }
+        else System.out.println("Невірний формат документа");
+
+    }
+
+    public static void handler(AbstractHandler abstractHandler) {
+        abstractHandler.change();
+        abstractHandler.open();
+        abstractHandler.change();
+        abstractHandler.save();
     }
 }
